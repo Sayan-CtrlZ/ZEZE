9bbf5b10a06944fb558f1a20986e76da5a1f0b25
Sayan Shil <officialsayan36@gmail.com>
2026-04-26 18:28:20 +0530

    Restructure for Render deployment

--- Client/next.config.ts
+++ Client/next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  rewrites: async () => {
-    if (process.env.NODE_ENV === "development") {
-      return [
-        {
-          source: "/api/:path*",
-          destination: "http://127.0.0.1:10000/api/:path*",
-        },
-      ];
-    }
-    return [];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
